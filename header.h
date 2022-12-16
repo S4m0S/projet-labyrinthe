0ecc8e71e2fbfdb9e276c3408876b47a0c84aa32
S4m0S <adamnasreddine@outlook.com>
2022-12-16 12:20:06 +0100

    typedef struct

--- header.h
+++ header.h
@@ -2,11 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct position{
+    int x;
+    int y;
+};
 
+typedef struct tresor{
+    position* piece;
+    bool find;
+    char affiche;
+};
 
 typedef struct {
     position* piece;
     bool trouve;
     char affichage;
     
-}
+};
