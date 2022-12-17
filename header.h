fe1c8625a0e4a316cf37e9a84bde52232c8751d1
S4m0S <91144929+S4m0S@users.noreply.github.com>
2022-12-17 22:23:32 +0000

    miam

--- header.h
+++ header.h
@@ -2,19 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define true 1
+#define false 0
+
 typedef char* string;
+typedef short int bool; 
 
 typedef struct position{
     int x;
     int y;
-};
+}position;
 
 typedef struct tresor{
     position* piece;
     bool find;
     char affiche;
     
-};
+}tresor;
 
 typedef struct joueur {
     position piece;
@@ -22,7 +26,7 @@ typedef struct joueur {
     char affiche;
     string nom;
     tresor listeTresor[];
-};
+}joueur;
 
 typedef struct tuile{
     int type;
@@ -30,4 +34,4 @@ typedef struct tuile{
     tresor treasure;
     bool oove;
     int orientation;
-};
+}tuile;
