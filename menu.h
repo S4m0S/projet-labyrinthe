a50171bd3e0136a3f0b4100c139b652e711d5ff7
MichaelAdda7 <michael.adda92@gmail.com>
2022-12-27 18:44:38 +0100

    bu

--- menu.h
+++ menu.h
@@ -4,8 +4,6 @@
 
 void menuInitial(){//soit on fait la declaration du choix ici
     int choix;//soit on déclare ici
-    bool running = true;
-    do{
     do{
     system("cls");
     printf("1. Nouvelle partie\n 2.Credits/Regles du jeu\n 3.Quitter\n");
@@ -21,19 +19,17 @@ void menuInitial(){//soit on fait la declaration du choix ici
             break;
         case 2:
             {
-            printf("Regles du jeu:\nLe Labyrinthe est un jeu de plateau comprenant 2 a 4 joueurs.\n Chaque joueur possède une liste de tresors
-            qu'il doit aller chercher dans un labyrinthe de 49 tuiles.\n Pour cela, chacun leur tour, les joueurs ont la possibilite de deplacer 
-            une rangee de tuiles et de placer une tuile au bout de cette rangee.\n Le joueur peut faire pivoter cette tuile. Enfin, le joueur peut 
+            printf("Regles du jeu:\nLe Labyrinthe est un jeu de plateau comprenant 2 a 4 joueurs. Chaque joueur possède une liste de tresors
+            qu'il doit aller chercher dans un labyrinthe de 49 tuiles. Pour cela, chacun leur tour, les joueurs ont la possibilite de deplacer 
+            une rangee de tuiles et de placer une tuile au bout de cette rangee. Le joueur peut faire pivoter cette tuile. Enfin, le joueur peut 
             deplacer son pion d'autant de case qu' il veut,tant qu' il suit un chemin. Le premier joueur qui recupere tout ses tresors gagne la partie.\
             Credits: Adda Michael - El Haj Jean - Hogommat Julien - Nasreddine Adam\n");
             }       
             break;
         case 3:
-            running = false;
             break;
             //jsp trop quoi mettre 
     }
-    }while(running==true);
 }
 
 void DemarrageMenu(char listeNomsJoueurs[],joueur listeJoueurs[],char listePionJoueurs[]){
@@ -54,12 +50,17 @@ void DemarrageMenu(char listeNomsJoueurs[],joueur listeJoueurs[],char listePionJ
         switch(nbPion){
             case 1:
                 listePionJoueurs[i]=listeRepresentation[0];
+                break;
             case 2:
                 listePionJoueurs[i]=listeRepresentation[1];
+                break;
             case 3:
                 listePionJoueurs[i]=listeRepresentation[2];
+                break;
             case 4:
                 listePionJoueurs[i]=listeRepresentation[3];
+                break;
+                
         }
         //pour la couleur vu qu'on la pas mis dans la structure joueur jsp    
 
